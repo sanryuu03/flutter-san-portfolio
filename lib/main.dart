ef88746921e08ec5add97f7a25e6062449d2a078
sanryuu03 <ryuuosku@gmail.com>
2022-07-31 23:52:48 +0700

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'san portfolio 2022'.capitalizeFirstofEach,
       theme: ThemeData(),
       home: Portfolio(),
