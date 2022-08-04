98215486c857f654af4fa23bd2ac93c559cd2e5d
sanryuu03 <ryuuosku@gmail.com>
2022-08-04 23:23:54 +0700

    fix layouts portfolio

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,67 @@ class Portfolio extends StatelessWidget {
                     Icon(Icons.flash_on_sharp),
                   ],
                 ),
+                Container(
+                  width: double.infinity,
+                  height: 350,
+                  decoration: BoxDecoration(
+                      color: Colors.cyan,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(40),
+                        bottom: Radius.circular(40),
+                      )),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text('skill'.capitalizeFirstofEach,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500)),
+                      Row(
+                        children: [
+                          SizedBox(width: 12),
+                          Image.asset('images/laravel.png'),
+                          Image.asset(
+                            'images/mysql.png',
+                            width: 100,
+                            height: 100,
+                          ),
+                          SizedBox(width: 100),
+                          Text('see more'.capitalizeFirstofEach,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500)),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(width: 10),
+                          Text(
+                            'kenapa aku membuat aplikasi ini ?'
+                                .capitalizeFirstofEach,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0, left: 55.5),
+                        child: Text(
+                          'tentu saja karna ini tugas submission.\n ternyata aku memang tidak ada bakat dibidang seni. \n ingin jadi fullstack ternyata tidak semudah kubayangkan. \n\n bingung mau bikin apa lagi, hahaha'
+                              .capitalizeFirstofEach,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
